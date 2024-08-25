1b5dec93c4993b1c464c5fe7b30b9d70bae93c88
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-08-25 20:24:13 +0300

    Done editing Chat page + added user icon

--- pages/tools/chat.vue
+++ pages/tools/chat.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="pb-16 pattern">
-    <div class="container relative mx-auto  px-4 min-h-screen  flex flex-col overflow-hidden">
-      <div class="flex justify-between items-center chat-header rounded-xl my-3 p-4 w-full lg:w-3/4 xl:w-1/2 mx-auto">
-        <div class="flex-1">
-          <nuxt-link to="/" title="back" class="back-btn w-8 h-8 md:w-12 md:h-12 rounded-lg flex justify-center items-center">
-            <i class="icon icon-arrow-ios-back icon-32"></i>
+    <div class="container relative mx-auto px-4 min-h-screen flex flex-col overflow-hidden">
+      <div class="flex justify-between items-center chat-header rounded-xl my-2 p-0 w-full lg:w-3/4 xl:w-1/2 mx-auto">
+        <!-- Back arrow -->
+        <div class="flex-1 pl-2">
+          <nuxt-link to="/" title="back" class="back-btn w-6 h-6 md:w-8 md:h-8 rounded-lg flex justify-center items-center">
+            <i class="icon icon-arrow-ios-back icon-16"></i>
           </nuxt-link>
         </div>
-        <div class="text-center flex-1">
-          <img class="block w-16 md:w-24 rounded-full mx-auto" :src="daraAvatar" draggable="false" />
+        
+        <!-- Chat with Dara text and Dara image -->
+        <div class="flex items-center justify-center text-white w-full">
+          <div class="flex items-center justify-center">
+            <span class="text-xl md:text-2xl mr-2 sm:text-lg">Chat with Dara</span>
+            <img class="block w-10 md:w-12 rounded-full" :src="daraAvatar" draggable="false" />
+          </div>
         </div>
-        <div class="flex-1"><</div>
-      </div>
-      <div  class="bg-white border border-[#5e2c57] rounded-lg p-2 relative flex-1 flex flex-col mb-2 w-full lg:w-3/4 xl:w-1/2 mx-auto">
+     </div>
+      <!-- Chat content -->
+      <div class="bg-white/40 border border-[#5e2c57]/50 rounded-xl p-2 relative flex-1 flex flex-col mb-2 w-full lg:w-3/4 xl:w-1/2 mx-auto">
         <div class="flex-1">
           <ul class="flex flex-col flex-1 overflow-y-auto hide-sb main-chat" ref="chatBody">
-            <li class="flex flex-col max-w-md my-2" :class="[message.role === 'user' ? 'self-end items-end' : 'self-start items-start']" v-for="message in messages" :key="message.id">
-              <img class="w-12 rounded-full" :src="message.role === 'ai' ? daraAvatar : '/avatar.png'" />
-              <div class="rounded-lg p-2 mt-1 text-white" :class="[message.role === 'ai' ? 'bg-[#1a1a2e]' : 'bg-[#5e2c57]']">
+            <li v-for="message in messages" :key="message.id" :class="['flex items-center max-w-md my-2', message.role === 'user' ? 'self-end flex-row-reverse' : 'self-start']">
+              <img class="w-12 rounded-full" :class="message.role === 'user' ? 'ml-3' : 'mr-3'" :src="message.role === 'ai' ? daraAvatar : userAvatar"/>
+              <div class="rounded-lg p-2 text-white" :class="[message.role === 'ai' ? 'bg-[#5e2c57]' : 'bg-[#1a1a2e]']">
                 <p>{{ message.text }}</p>
               </div>
             </li>
             <li v-if="typing" class="flex flex-col max-w-md self-start items-start">
               <img class="w-12 rounded-full" :src="daraAvatar" />
-              <div class="bg-[#1a1a2e] rounded-lg p-2 mt-1 text-white">
+              <div class="bg-[#5e2c57] rounded-lg p-2 mt-1 text-white">
                 <p>typing ...</p>
               </div>
             </li>
@@ -32,8 +38,8 @@
         <form @submit.prevent="sendMessage" class="container">
           <div class="container p-4">
             <div class="flex bg-white">
-              <textarea v-model="message" class="flex-1 p-3 border rounded-lg text-black" rows="2" placeholder="your message"></textarea>
-              <button :disabled="typing || !message" :class="[typing ? 'cursor-not-allowed opacity-40' : '']" type="submit" title="send" class="flex justify-center items-center bg-[#af3d50] hover:opacity-75 transition-opacity text-white px-3 rounded-lg ml-2 cursor-pointer"><i class="icon icon-lg rotate-45 icon-paper-plane"></i></button>
+              <textarea v-model="message" @keydown="handleKeydown" class="flex-1 p-0 border rounded-xl text-black focus:border-red-500" rows="2" placeholder="your message"></textarea>
+              <button :disabled="typing || !message" :class="[typing ? 'cursor-not-allowed opacity-40' : '']" type="submit" title="send" class="flex justify-center items-center bg-[#af3d50] hover:opacity-75 transition-opacity text-white px-3 rounded-xl ml-2 cursor-pointer"><i class="icon icon-lg rotate-45 icon-paper-plane"></i></button>
             </div>
           </div>
         </form>
@@ -41,6 +47,7 @@
     </div>
   </div>
 </template>
+
 <script>
 import {nanoid} from 'nanoid'
 import {reactive, ref, unref} from 'vue'
@@ -56,13 +63,14 @@ export default {
     const runtimeConfig = useRuntimeConfig()
     const toast = useToast()
     const daraAvatar = ref('/dara.png')
+    const userAvatar = ref('/user.png')
     const session = nanoid(7)
     let chatBody = ref(null)
     const messages = reactive([
       {
         id: nanoid(8),
         role: 'ai',
-        text: 'Hi my name is Dara, How i can help you today ?'
+        text: 'Hi Im Dara, How i can help you today ?'
       }
     ])
     let message = ref('')
@@ -107,14 +115,24 @@ export default {
         typing.value = false
       }
     }
+
+    const handleKeydown = (event) => {
+      if (event.key === 'Enter' && !event.shiftkey) {
+        event.preventDefault()
+        sendMessage()
+      }
+    }
+
     return {
       messages,
       daraAvatar,
+      userAvatar,
       message,
       chatBody,
       sendMessage,
       layoutConfig,
-      typing
+      typing,
+      handleKeydown
     }
   },
   created() {
@@ -124,6 +142,7 @@ export default {
   }
 }
 </script>
+
 <style scoped>
 .main-chat {
   max-height: calc(100vh - 18.5rem);
@@ -141,10 +160,15 @@ export default {
   display: none; /* Safari and Chrome */
 }
 .chat-header {
-  background-color: #1a1a2e;
+  background-color: #5e2c57;
 }
 .back-btn {
   background-color: #af3d50;
   color: #fff;
 }
+
+li.flex.items-center {
+  align-items: flex-end; /* Align image and text vertically at the top */
+}
+
 </style>
